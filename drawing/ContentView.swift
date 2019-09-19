bd23bbdb7e9823004661696e853ff8539f64d26b
yahi61006 <yahi61006@gmail.com>
2019-09-19 17:36:00 +0800

    Initial Commit

---
+++ drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  drawing
+//
+//  Created by Kumo on 2019/9/19.
+//  Copyright © 2019 Kumo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
